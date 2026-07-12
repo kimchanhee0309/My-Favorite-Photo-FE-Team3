2e3f565214f94a8791523fa0da35115a9a12c4fa
MunChiho <mch52373820@gmail.com>
2026-07-13 02:01:45 +0900

    fix: globals.css 통합 및 디자인 토큰 오타 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
