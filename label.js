124ce8706288aa7738763e04aff129ea21ac1db5
apple <apple@appleui-MacBookAir.local>
2026-07-13 09:59:46 +0900

    chore: label template setting

---
+++ label.js
@@ -0,0 +1,10 @@
+import githubLabelSync from "github-label-sync";
+
+githubLabelSync({
+  accessToken: process.env.GITHUB_TOKEN,
+  repo: "kimchanhee0309/My-Favorite-Photo-FE-Team3",
+  labels: [],
+  dryRun: true,
+}).then((diff) => {
+  console.log(diff);
+});
