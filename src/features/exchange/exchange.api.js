e7de89cf2843d2e59c96821f019dace2aeeef891
apple <apple@appleui-MacBookAir.local>
2026-07-13 21:34:23 +0900

    feat: exchange.api.js 파일 작업

---
+++ src/features/exchange/exchange.api.js
@@ -0,0 +1,27 @@
+import { apiClient } from "@/common/api/client";
+
+export function createExchangeOffer(shopListingId, data) {
+  return apiClient(`/shop-listings/${shopListingId}/exchanges`, {
+    method: "POST",
+    body: JSON.stringify({
+      photocardId: data.photocardId,
+      offeredQuantity: Number(data.offeredQuantity || 1),
+    }),
+  });
+}
+
+export function getReceivedExchanges() {
+  return apiClient("/exchanges/received");
+}
+
+export function acceptExchange(exchangeId) {
+  return apiClient(`/exchanges/${exchangeId}/accept`, {
+    method: "PATCH",
+  });
+}
+
+export function rejectExchange(exchangeId) {
+  return apiClient(`/exchanges/${exchangeId}/reject`, {
+    method: "PATCH",
+  });
+}
