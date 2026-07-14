510282f0d939eb6a860fb60807907766647c67b0
apple <apple@appleui-MacBookAir.local>
2026-07-14 16:14:14 +0900

    chore: 모서리(rounded-xs)와 여백(px-3, py-5) 수정

--- src/common/components/Input/Textarea.jsx
+++ src/common/components/Input/Textarea.jsx
@@ -26,7 +26,7 @@ export default function Textarea({
       <textarea
         id={textareaId}
         disabled={disabled}
-        className={`w-[345px] resize-none rounded-sm border bg-gray-500 px-5 py-[18px] 
+        className={`w-[345px] resize-none rounded-xs border bg-gray-500 px-3 py-5 
           transition-colors md:w-[440px] lg:w-[520px] h-[180px]
           typo-14-light lg:typo-16-light text-white outline-none 
           placeholder:text-gray-200 disabled:cursor-not-allowed
