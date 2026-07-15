13fb593cbf158b741f17a967edddb052d5aca860
ezzzni <unin5412@gmail.com>
2026-07-15 15:37:49 +0900

    feat: header 컴포넌트 M, L 사이즈 구현 완료

--- src/common/components/header/MobileMenuDrawer.jsx
+++ src/common/components/header/MobileMenuDrawer.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import UserMenuList from "./UserMenuList";
 
 export default function MobileMenuDrawer({
   isOpen,
@@ -19,42 +19,7 @@ export default function MobileMenuDrawer({
       />
 
       <div className="absolute top-0 left-0 w-65 h-full bg-gray-500 shadow-[0_0_20px_0_rgba(0,0,0,0.20)] pt-10 pb-5 flex flex-col justify-between">
-        <div>
-          <div className="flex w-65 h-20.75 flex-col justify-center items-center gap-5">
-            <div className="flex w-55 h-15.75 flex-col items-start gap-5 shrink-0">
-              <p className="typo-18-bold text-white">
-                안녕하세요, {user.name}님!
-              </p>
-              <div className="flex items-center justify-between w-full">
-                <span className="typo-12-light text-gray-300">보유 포인트</span>
-                <span className="typo-12-regular text-main">
-                  {user.point.toLocaleString()} P
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <svg
-            width="260"
-            height="1"
-            viewBox="0 0 260 1"
-            fill="none"
-            className="block mt-5">
-            <path opacity="0.5" d="M0 0.5H260" stroke="#5A5A5A" />
-          </svg>
-
-          <nav className="flex flex-col w-27.5 h-21.75 gap-3.75 whitespace-nowrap ml-5 mt-6">
-            {menuItems.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                onClick={onClose}
-                className="typo-14-bold text-white">
-                {item.label}
-              </Link>
-            ))}
-          </nav>
-        </div>
+        <UserMenuList user={user} menuItems={menuItems} onItemClick={onClose} />
 
         <button
           onClick={onLogout}
