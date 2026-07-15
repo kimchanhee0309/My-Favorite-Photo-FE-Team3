ba1538cd93fe9b0b89f740cb27b2b48e835ca688
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-15 09:58:01 +0900

    chore: main page.jsx 원상복구

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,20 +1,3 @@
-import PhotoCard from "@/common/components/PhotoCard/PhotoCard";
-
 export default function page() {
-  const mockCard = {
-    title: "테스트 제목",
-    grade: "COMMON",
-    genre: "TRAVEL",
-    description: "테스트 설명",
-    pricePerUnit: 5,
-    // quantity: 5,
-    // remainingQuantity: 3,
-    status: "ON_SALE",
-    imageUrl: "/default.png",
-    saleType: "SALE",
-    quantityText: "3 / 5", //실제로는 quantity와 remainingQuantity활용해 작성
-    quantityLabel: "잔여",
-    // purchaseText: "4 P 에 구매",
-  };
-  return <PhotoCard card={mockCard} />;
+  return <div>랜딩페이지</div>;
 }
