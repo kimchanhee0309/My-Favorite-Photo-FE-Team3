96bf2705d890deb370d9b820e2bbe708906f78f6
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-19 01:18:08 +0900

    feat: 마이갤러리 카드 목록 구현

---
+++ src/app/(main)/gallery/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GalleryContent from "./_components/GalleryContent";
+
+export default function page() {
+  return (
+    <div>
+      <GalleryContent />
+    </div>
+  );
+}
