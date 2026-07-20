b4ba0e016b5be66526716ee67fedff7219a214da
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-20 11:43:39 +0900

    feat: MyCard에 size prop 추가 및 고정 사이즈(sizeStyle) 지원

--- src/common/components/photocard/PhotoCard.constants.js
+++ src/common/components/photocard/PhotoCard.constants.js
@@ -14,6 +14,19 @@ export const cardStyle = {
     "left-1 top-1 px-1.5 typo-10-regular md:left-2.5 md:top-2.5 md:typo-16-regular",
 };
 
+export const sizeStyle = {
+  container: "p-5 gap-6 w-86",
+  divider: "pb-4",
+  dividerRow: "flex justify-between whitespace-nowrap",
+  title: "typo-22-bold",
+  labelLight: "typo-16-light",
+  labelRegular: "typo-16-regular",
+  valueRegular: "typo-18-regular",
+  image: "relative w-[300px] h-[210px]",
+  logo: "self-center",
+  badge: "left-2.5 top-2.5 px-1.5 typo-10-regular",
+};
+
 export const gradeColor = {
   COMMON: "text-main",
   RARE: "text-blue",
