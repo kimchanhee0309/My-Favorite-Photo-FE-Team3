7eacd5ca67cb933837cd3b3f6b2a312bd2d94291
MunChiho <mch52373820@gmail.com>
2026-07-21 15:46:53 +0900

    feat: 공용 ConfirmModal/CustomModal 및 ResultSection 컴포넌트 추가
    결과 페이지와 교환 모달에서 재사용할 공용 UI를 먼저 정비하고,
    Drawer/FullScreenModal 스타일과 Dropdown export 경로를 함께 수정

--- src/common/components/modal/Drawer.jsx
+++ src/common/components/modal/Drawer.jsx
@@ -9,7 +9,7 @@ export default function Drawer({ open, onClose, children }) {
         <VaulDrawer.Overlay className="fixed inset-0 z-50 bg-black/80" />
         <VaulDrawer.Content className="fixed right-0 bottom-0 left-0 z-50 flex max-h-[90vh] flex-col items-center rounded-t-xs bg-gray-500 outline-none">
           <div className="mt-4 h-1 w-10 shrink-0 rounded-full bg-gray-400" />
-          <div className="w-[345px] flex-1 overflow-y-auto px-5 py-6 md:w-[704px]">
+          <div className="w-[375px] flex-1 overflow-y-auto px-[15px] py-6 md:w-[744px] md:px-5">
             {children}
           </div>
         </VaulDrawer.Content>
