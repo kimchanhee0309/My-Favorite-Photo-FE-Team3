7ce60cd5e41b52eaf6fe646e0c8adecaaf691198
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-21 10:14:51 +0900

    chore: 불필요한 style 제거

--- src/app/(main)/marketplace/page.jsx
+++ src/app/(main)/marketplace/page.jsx
@@ -3,7 +3,7 @@ import MarketPlaceHeader from "./_components/MarkeyPlaceHeader";
 
 export default function MarketplacePage() {
   return (
-    <div className="layout-container flex flex-col py-10">
+    <div>
       <MarketPlaceHeader />
       <MarketPlaceContent />
     </div>
