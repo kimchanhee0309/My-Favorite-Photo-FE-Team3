46543131a02576b847674455085729a220fbcbda
ezzzni <unin5412@gmail.com>
2026-07-21 14:01:21 +0900

    feat: 랜덤 포인트 선택 모달 구현

---
+++ src/features/point/constants.js
@@ -0,0 +1,5 @@
+export const RANDOM_BOXES = [
+  { id: 1, src: "/random_box_blue.png", alt: "파란색 랜덤 상자" },
+  { id: 2, src: "/random_box_purple.png", alt: "보라색 랜덤 상자" },
+  { id: 3, src: "/random_box_pink.png", alt: "핑크색 랜덤 상자" },
+];
