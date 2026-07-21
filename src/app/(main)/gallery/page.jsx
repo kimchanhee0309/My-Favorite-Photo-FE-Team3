0353248c144434e71cf6f1ba9a68069ac39d692f
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-21 09:55:18 +0900

    chore: Title import 경로 오탈자 및 gallery page 함수명 수정

--- src/app/(main)/gallery/page.jsx
+++ src/app/(main)/gallery/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GalleryContent from "./_components/GalleryContent";
 import GalleryHeader from "./_components/GalleryHeader";
 
-export default function page() {
+export default function GalleryPage() {
   return (
     <div>
       <GalleryHeader />
--- src/common/components/index.js
+++ src/common/components/index.js
@@ -10,4 +10,4 @@ export { default as Textarea } from "./input/Textarea";
 export { default as ImageUpload } from "./input/ImageUpload";
 // export { default as BottomSheetFilter } from './BottomSheetFilter/BottomSheetFilter';
 // export { default as Modal } from './Modal';
-export { default as Title } from "./Title";
+export { default as Title } from "./title/Title";
