2557ec75c38b96f8cf9152150b61bd925cb549af
ezzzni <unin5412@gmail.com>
2026-07-23 11:51:06 +0900

    chore: API 연동 전 임시 처리

--- src/features/notification/components/MobileNotificationOverlay.jsx
+++ src/features/notification/components/MobileNotificationOverlay.jsx
@@ -7,6 +7,7 @@ export default function MobileNotificationOverlay({ isOpen, onClose }) {
 
   if (!isOpen) return null;
 
+  //TODO: 알림 클릭시 상세 페이지 라우팅되도록 API 구현 후 연결
   return (
     <div className="fixed inset-x-0 top-20 bottom-0 z-30 overflow-hidden bg-gray-500 md:hidden">
       <NotificationList
