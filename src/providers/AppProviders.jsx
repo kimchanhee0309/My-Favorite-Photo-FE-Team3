a82a10013c7093d14249ce94171cf765cbf84621
ezzzni <unin5412@gmail.com>
2026-07-27 18:19:31 +0900

    fix: RandomBoxModalProviderWithAuth를 이용하여 RBMProvider에 user의 로그인, 로딩 상태를 전달받을 수 있게끔 수정

--- src/providers/AppProviders.jsx
+++ src/providers/AppProviders.jsx
@@ -2,13 +2,32 @@
 
 import { ModalProvider } from "./ModalProvider";
 import QueryProvider from "./QueryProvider";
-import { AuthProvider } from "./AuthProvider";
+import { AuthProvider, useAuth } from "./AuthProvider";
+import { RandomBoxModalProvider } from "./RandomBoxModalProvider";
+
+function RandomBoxModalProviderWithAuth({ children }) {
+  const { user, isLoggedIn, isLoading } = useAuth();
+
+  if (isLoading) {
+    return children;
+  }
+
+  return (
+    <RandomBoxModalProvider userId={isLoggedIn ? user.id : undefined}>
+      {children}
+    </RandomBoxModalProvider>
+  );
+}
 
 export default function AppProviders({ children }) {
   return (
     <QueryProvider>
       <AuthProvider>
-        <ModalProvider>{children}</ModalProvider>
+        <ModalProvider>
+          <RandomBoxModalProviderWithAuth>
+            {children}
+          </RandomBoxModalProviderWithAuth>
+        </ModalProvider>
       </AuthProvider>
     </QueryProvider>
   );
