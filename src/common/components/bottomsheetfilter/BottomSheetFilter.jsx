0460d035b47abe425960f7c0197db45cb08c730b
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-27 18:16:33 +0900

    fix: 바텀시트 필터 탭 제목 좁은 화면에서 잘리지 않도록 가로 스크롤 처리

--- src/common/components/bottomsheetfilter/BottomSheetFilter.jsx
+++ src/common/components/bottomsheetfilter/BottomSheetFilter.jsx
@@ -83,12 +83,12 @@ export default function BottomSheetFilter({
               <Image src="/close.svg" width={13} height={13} alt="" />
             </button>
           </header>
-          <ul className="flex gap-6 px-6">
+          <ul className="flex [scrollbar-width:none] gap-6 overflow-x-auto px-6 whitespace-nowrap [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
             {orderedTabs.map((tabName) => (
               <li key={tabName}>
                 {selectedTab !== tabName ? (
                   <button
-                    className="typo-14-regular px-4 py-4 text-gray-400"
+                    className="typo-14-regular px-4 py-4 whitespace-nowrap text-gray-400"
                     onClick={() => setSelectedTab(tabName)}>
                     {tabText[tabName]}{" "}
                     {selectedOptions[tabName].length
@@ -97,7 +97,7 @@ export default function BottomSheetFilter({
                   </button>
                 ) : (
                   <button
-                    className="border-b border-white px-4 py-4 text-[14px] font-medium text-white"
+                    className="border-b border-white px-4 py-4 text-[14px] font-medium whitespace-nowrap text-white"
                     onClick={() => setSelectedTab(tabName)}>
                     {tabText[tabName]}{" "}
                     {selectedOptions[tabName].length
