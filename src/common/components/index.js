24283f8cdf9d33e9a5990a32ce5aa757459713b1
apple <apple@appleui-MacBookAir.local>
2026-07-29 15:48:00 +0900

    대문자->소문자로 수정(빌드 과정 속 에러 원인)

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -5,8 +5,8 @@ export { default as SearchInput } from "./input/SearchInput";
 
 // 완성되면 이 파일 담당자(문치호)에게 알려주세요 - 아래 형식으로 한 줄씩 추가합니다.
 export { default as Stepper } from "./stepper/Stepper";
-export { default as Dropdown } from './dropdown/Dropdown';
+export { default as Dropdown } from "./dropdown/Dropdown";
 export { default as Textarea } from "./input/Textarea";
 export { default as ImageUpload } from "./input/ImageUpload";
-export { default as BottomSheetFilter } from "./BottomSheetFilter/BottomSheetFilter";
+export { default as BottomSheetFilter } from "./bottomSheetFilter/BottomSheetFilter";
 export { default as Title } from "./title/Title";
